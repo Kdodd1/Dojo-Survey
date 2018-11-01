b32d8bac998cbc01cf509c8b195d6fdbf16db328
Kyle Dodd <Kylepdodd1@gmail.com>
2018-11-01 19:16:46 -0400

    first commit

---
+++ dojoSurvey.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+# our index route will handle rendering our formcopy
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/result', methods=['POST'])
+def create_user():
+    print("Got Post Info")
+
+    name = request.form['name']
+    location = request.form['location']
+    favorite = request.form['favorite']
+    comment = request.form['comment']
+
+    return render_template("result.html")
+
+@app.route('/danger')
+def danger():
+	print("a user tried to vist /danger")
+	return redirect("/")
+
+
+
+if __name__=="__main__":
+    app.run(debug=True) 
